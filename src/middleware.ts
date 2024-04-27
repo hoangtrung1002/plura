55e99f9e780a9a0ae9f4171f2e752cdf628802fa
hoangtrung1002 <n.jordan1002@gmail.com>
2024-04-27 14:56:54 +0700

    setup project

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({ publicRoutes: ["/site", "/api/uploadthing"] });
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
