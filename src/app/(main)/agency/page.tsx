8833fee537ca7dcde53e9a3d17e981a77d32bb4c
hoangtrung1002 <n.jordan1002@gmail.com>
2024-04-29 00:59:45 +0700

    feat: setup subdomain in middleware file

---
+++ src/app/(main)/agency/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AgencyPate = () => {
+  return <div>AgencyPate</div>;
+};
+
+export default AgencyPate;
---
+++ src/app/[domain]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>dynamic domain</div>;
+};
+
+export default page;
